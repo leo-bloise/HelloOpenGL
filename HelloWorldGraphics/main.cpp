219bebaea78f64321a84cbec0efef2c62182214b
Leonardo Bloise <leonardocsbloise@gmail.com>
2025-10-07 23:38:16 -0300

    wip: created vertex shader and checked compilation

--- HelloWorldGraphics/main.cpp
+++ HelloWorldGraphics/main.cpp
@@ -2,6 +2,41 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+const char* vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main() {\n"
+	"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+
+bool checkCompilationShaderSuccess(unsigned int shaderId) {
+	int success;
+
+	glGetShaderiv(shaderId, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		char info[512];
+		glGetShaderInfoLog(shaderId, 512, NULL, info);
+		std::cerr << "SHADER COMPILATION ERROR: " << info;
+		return false;
+	}
+
+	return true;
+}
+
+unsigned int createVertexShader() {
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	if (!checkCompilationShaderSuccess(vertexShader)) {
+		return 0;
+	}
+
+	return vertexShader;
+}
+
 GLFWwindow* configureAsCurrentAndCreateWindow() {
 	if (!glfwInit()) {
 		std::cerr << "ERROR WHILE INITIATING GLFW";
@@ -42,12 +77,23 @@ void drawTriangle() {
 		0.0f, 0.5f, 0.0f
 	};
 
+	unsigned int VBO;
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 9, vertices, GL_STATIC_DRAW);
 }
 
 int main() {	
 	GLFWwindow* window = configureAsCurrentAndCreateWindow();
 
 	if (window == NULL) {
+		return -1;
+	}
+
+	unsigned int vertexShader = createVertexShader();
+
+	if (vertexShader == 0) {
 		return -1;
 	}
 
